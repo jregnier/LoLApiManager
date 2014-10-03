71f083d2c104dc36fe5d7ce19e8607ab8adf8f0f
jregnier <jason.regnier4@gmail.com>
2014-10-02 23:14:11 -0400

    Added PlayerDto file...

---
+++ Team7.LoLAPIManager/Dto/Game/PlayerDto.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="PlayerDto.cs" company="Team7 Productions">
+//     Copyright (c) 2014. All rights reserved.
+// </copyright>
+// <author>Jason Regnier</author>
+namespace Team7.LoLAPIManager.Dto.Game
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class PlayerDto
+    {
+        [DataMember(Name = "championId")]
+        public int ChampionId
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "summonerId")]
+        public long SummonerId
+        {
+            get;
+            set;
+        }
+
+        [DataMember(Name = "teamId")]
+        public int TeamId
+        {
+            get;
+            set;
+        }
+    }
+}
